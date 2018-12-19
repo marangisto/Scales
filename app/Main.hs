880b99df86e1f38a1408531d2761987f4b021be4
Marten Agren <marangisto@me.com>
2018-12-19 20:33:50 +0800

    Added mode class and pentatonic scales.

--- app/Main.hs
+++ app/Main.hs
@@ -6,19 +6,36 @@ type MIDI = Int
 
 data Note = C | C' | D | D' | E | F | F' | G | G' | A | A' | B deriving (Eq, Ord, Enum, Bounded)
 
+showSharp :: Note -> String
+showSharp C  = "C"
+showSharp C' = "C#"
+showSharp D  = "D"
+showSharp D' = "D#"
+showSharp E  = "E"
+showSharp F  = "F"
+showSharp F' = "F#"
+showSharp G  = "G"
+showSharp G' = "G#"
+showSharp A  = "A"
+showSharp A' = "A#"
+showSharp B  = "B"
+
+showFlat :: Note -> String
+showFlat C  = "C"
+showFlat C' = "Db"
+showFlat D  = "D"
+showFlat D' = "Eb"
+showFlat E  = "E"
+showFlat F  = "F"
+showFlat F' = "Gb"
+showFlat G  = "G"
+showFlat G' = "Ab"
+showFlat A  = "A"
+showFlat A' = "Bb"
+showFlat B  = "B"
+
 instance Show Note where
-    show C  = "C"
-    show C' = "C#"
-    show D  = "D"
-    show D' = "D#"
-    show E  = "E"
-    show F  = "F"
-    show F' = "F#"
-    show G  = "G"
-    show G' = "G#"
-    show A  = "A"
-    show A' = "A#"
-    show B  = "B"
+    show = showSharp
 
 type Octave = Int
 
@@ -27,7 +44,23 @@ newtype Pitch = Pitch { unPitch :: (Note, Octave) }
 instance Show Pitch where
     show (Pitch { unPitch = (n, o) }) = show n ++ show o
 
-data Mode
+class Enum a => Mode a where
+    intervals :: a -> [Int]
+    generate :: Int -> a -> [Int]
+    generate n m = scanl (+) n $ iterate rotate (intervals m)!!(fromEnum m)
+
+data Pentatonic
+    = Minor
+    | Major
+    | Egyptian
+    | ManGong
+    | Ritsusen
+    deriving (Eq, Ord, Enum, Bounded, Show)
+
+instance Mode Pentatonic where
+    intervals _ = [ 3, 2, 2, 3, 2 ]
+
+data Heptatonic
     = Ionian
     | Dorian
     | Phrygian
@@ -36,6 +69,9 @@ data Mode
     | Aeolian
     | Locrian
     deriving (Eq, Ord, Enum, Bounded, Show)
+
+instance Mode Heptatonic where
+    intervals _ = [ 2, 2, 1, 2, 2, 2, 1 ]
 
 pitch :: Note -> Octave -> Pitch
 pitch n o = Pitch (n, o)
@@ -46,17 +82,11 @@ fromMIDI d = let (i, j) = (d - 12) `divMod` 12 in Pitch (toEnum j, i)
 toMIDI :: Pitch -> MIDI
 toMIDI p = let (n, o) = unPitch p in 12 + fromEnum n + o * 12
 
-intervals :: [Int]
-intervals = [ 2, 2, 1, 2, 2, 2, 1 ]
-
 rotate :: [Int] -> [Int]
 rotate (x:xs) = xs ++ [ x ]
 
-generate :: Int -> Int -> [Int]
-generate n m = scanl (+) n $ iterate rotate intervals!!m
-
-scale :: Pitch -> Mode -> [Pitch]
-scale pitch mode = map fromMIDI . generate (toMIDI pitch) $ fromEnum mode
+scale :: Mode a => Pitch -> a -> [Pitch]
+scale pitch mode = map fromMIDI . generate (toMIDI pitch) $ mode
 
 freq :: MIDI -> Double
 freq d = 440*2**((fromIntegral d - 69)/12)
@@ -78,3 +108,4 @@ main = do
         [ toMIDI (pitch A 3) .. toMIDI (pitch A 5) ]
     putStrLn ""
     mapM_ (\m -> print (m,  scale (pitch C 4) m)) [Ionian .. Locrian]
+    mapM_ (\(m, d) -> print (m,  scale (pitch d 4) m)) $ zip [Minor .. Ritsusen] [D', F', G', A', C']
